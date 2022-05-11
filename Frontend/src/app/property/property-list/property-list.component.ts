9d067759511c150f8835876e85522c34340a3d79
jay-psspl <jay@prakashinfotech.com>
2022-05-11 10:56:24 +0530

    Property-card design

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Adani Housing",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Birla Housing",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":3,
+      "Name":"Reliance Housing",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":4,
+      "Name":"Ganesh Housing",
+      "Type":"House",
+      "Price":12000
+    }
+]
+
   constructor() { }
 
   ngOnInit(): void {
