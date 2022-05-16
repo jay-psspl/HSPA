76e912e1c2d94687763c54e6bc1aec4038c1d7fb
jay-psspl <jay@prakashinfotech.com>
2022-05-16 14:22:53 +0530

    Image Gallery

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,15 +23,19 @@ import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserServiceService } from './services/user-service.service';
 import { AlertifyService } from './services/alertify.service';
 import { AuthService } from './services/auth.service';
+import { PropertyDetailResolverService } from './property/property-detail/property-detail-resolver.service';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
-  {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'property-detail/:id', component: PropertyDetailComponent, resolve: {prp: PropertyDetailResolverService}},
   {path: 'user/login', component: UserLoginComponent},
   {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: PropertyListComponent}
+
+
+
 ]
 
 @NgModule({
@@ -46,8 +51,6 @@ const appRoutes: Routes = [
     AddPropertyComponent
 
 
-
-
    ],
   imports: [
     BrowserModule,
@@ -60,14 +63,16 @@ const appRoutes: Routes = [
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     ButtonsModule.forRoot(),
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    NgxGalleryModule
 
   ],
   providers: [
     HousingService,
     UserServiceService,
     AlertifyService,
-    AuthService
+    AuthService,
+    PropertyDetailResolverService
   ],
   bootstrap: [AppComponent]
 })
--- Frontend/src/app/property/property-detail/property-detail.component.ts
+++ Frontend/src/app/property/property-detail/property-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Property } from 'src/app/model/property';
 import { HousingService } from 'src/app/services/housing.service';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-property-detail',
@@ -11,25 +14,70 @@ import { HousingService } from 'src/app/services/housing.service';
 export class PropertyDetailComponent implements OnInit {
 public propertyId: number;
 property = new Property();
+galleryOptions: NgxGalleryOptions[];
+galleryImages: NgxGalleryImage[];
 
   constructor(private route: ActivatedRoute, private router: Router, private housingService: HousingService) { }
 
   ngOnInit() {
 
     this.propertyId = +this.route.snapshot.params['id'];
-
-    this.route.params.subscribe(
-      (params) => {
-        this.propertyId = +params['id'];
-
-        this.housingService.getProperty(this.propertyId).subscribe(
-
-          (data: Property) => {
-
-            this.property = data;
-          }
-        );
+    this.route.data.subscribe(
+      (data: Property) => {
+        this.property = data['prp'];
       }
     );
+
+    // this.route.params.subscribe(
+    //   (params) => {
+    //     this.propertyId = +params['id'];
+
+    //     this.housingService.getProperty(this.propertyId).subscribe(
+
+    //       (data: Property) => {
+
+    //         this.property = data;
+    //       }, error => this.router.navigate(['/'])
+    //     );
+    //   }
+    // );
+
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '465px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: true
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/A-1.jpg',
+        medium: 'assets/images/A-1.jpg',
+        big: 'assets/images/A-1.jpg'
+      },
+      {
+        small: 'assets/images/A-2.jpg',
+        medium: 'assets/images/A-2.jpg',
+        big: 'assets/images/A-2.jpg'
+      },
+      {
+        small: 'assets/images/A-3.jpg',
+        medium: 'assets/images/A-3.jpg',
+        big: 'assets/images/A-3.jpg'
+      },
+      {
+        small: 'assets/images/A-4.jpg',
+        medium: 'assets/images/A-4.jpg',
+        big: 'assets/images/A-4.jpg'
+      },
+      {
+        small: 'assets/images/A-5.jpg',
+        medium: 'assets/images/A-5.jpg',
+        big: 'assets/images/A-5.jpg'
+      },
+    ];
   }
 }
